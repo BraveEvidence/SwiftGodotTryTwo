b492da6103453f47a9a57c9a35c61857d36e1769
Pritish <pritish@transformhub.com>
2024-12-02 20:25:05 +0530

    first commit

---
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,23 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftGodot
+
+
+#initSwiftExtension(
+    cdecl: "swift_entry_point",
+    types: [MyLibrary.self]
+)
+
+@Godot // If Godot needs to see the class, it needs this bit
+class MyLibrary {
+    @Callable // If Godot needs to execute a function, you need this bit
+    func addition(a:Int, b:Int) -> Int {
+        a+b
+    }
+
+    @Callable
+    func hello(input:String) -> String {
+        "Hello " + input
+    }
+}
